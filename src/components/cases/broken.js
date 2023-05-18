70c54ccce8a0fa93bb8f1095a0fa864bd22c2790
dashzt <dashzt@gmail.com>
2023-05-19 02:42:34 +0300

    update cases

--- src/components/cases/broken.js
+++ src/components/cases/broken.js
@@ -1,4 +1,4 @@
-import { Red50, Blue100 } from '../boxes'
+import { Red50, Blue50, Blue100 } from '../boxes'
 
 export const Broken = () => {
   return <div>
@@ -9,7 +9,10 @@ export const Broken = () => {
     <Blue100>
       Blue 100
     </Blue100>
+    <Blue50>
+      Blue 50
+    </Blue50>
     <br />
-    <span>If you take a closer look on Blue 100 box, you can see it actually has a class which has background: blue, but it is put into styles tag in wrong order</span>
+    <span>If you take a closer look on Blue 100 and Blue 50 boxes, you can see it actually has a class which has background: blue, but it is put into styles tag in wrong order</span>
   </div>
 }
--- src/components/cases/working.js
+++ src/components/cases/working.js
@@ -1,8 +1,11 @@
-import { Blue50, Blue100 } from '../boxes'
+import { Red50, Blue50, Blue100 } from '../boxes'
 
 export const Working = () => {
   return <div>
     <h3>Working case</h3>
+    <Red50>
+      Red 50
+    </Red50>
     <Blue50>
       Blue 50
     </Blue50>
